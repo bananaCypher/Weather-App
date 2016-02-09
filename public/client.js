74ce9b1fecf65b7b89f2ac350369b9657d37526e
Callum Hogg <bananaCypher@gmail.com>
2016-02-09 18:21:59 +0000

    done

---
+++ public/client.js
@@ -0,0 +1,128 @@
+City = function(name){
+  this.name = name;
+  this.url = '/weather/' + this.name; 
+  this.data;
+}
+City.prototype = {
+  getWeatherData: function(callback){
+    var request = new XMLHttpRequest();
+    request.open('GET', this.url);
+    request.onload = function(){
+      if (request.status === 200) {
+        this.data = JSON.parse(request.responseText); 
+        callback();
+      } else {
+        console.log('AJAX request failed to ' + this.url); 
+      }
+    }.bind(this);
+    request.send(null);
+  }
+}
+
+var getIconURL = function(icon){
+  return 'http://openweathermap.org/img/w/' + icon + '.png'
+}
+
+var displayWeatherData = function(city){
+  var container = document.getElementById('weather-details');
+  var heading = document.getElementById('weather-details-heading');
+  var body = document.getElementById('weather-details-body')
+
+  var wind = city.data.wind;
+  var weather = city.data.weather[0];
+  var icon = getIconURL(weather.icon);
+  var main = city.data.main;
+    
+  container.style.display = 'block';
+  heading.innerText = city.data.name;
+  htmlString = "<p><img src='" + icon + "'><b>" + weather.main + "</b>, " + weather.description + "</p>";
+  htmlString += "<p>Wind speed: " + wind.speed + "</p>"
+  htmlString += "<table><thead><tr><th>Metric</th><th>Value</th></tr></thead>";
+  htmlString += "<tr><td>Temprature</td><td>" + main.temp + "</td></tr>";
+  htmlString += "<tr><td>Pressure</td><td>" + main.pressure + "</td></tr>";
+  htmlString += "<tr><td>Humidity</td><td>" + main.humidity + "</td></tr>";
+  htmlString += "</table>"
+  body.innerHTML = htmlString;
+  var button = document.createElement('button');
+  if (JSON.parse(localStorage.getItem('city')) == city.data.name) {
+      button.disabled = true;
+      button.innerText = 'City has been saved';
+      button.onclick = null;
+  } else {
+    button.innerText = 'Save city';
+    button.onclick = function(){
+      localStorage.setItem('city', JSON.stringify(city.name)); 
+      button.disabled = true;
+      button.innerText = 'City has been saved';
+      button.onclick = null;
+    };
+  }
+  body.appendChild(button);
+
+  var position = {lat: city.data.coord.lat, lng: city.data.coord.lon};
+  var mapCanvas = document.getElementById('map');
+  var mapOptions = {
+    center: position,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({
+    position: position,
+    map: map,
+  }); 
+};
+
+var setAllCursors = function(cursor){
+  nodes = document.childNodes;
+  for (var i = 0, len = nodes.length; i < len; i++) {
+    try {
+      nodes[i].style.cursor = cursor; 
+    }
+    catch(err) {
+    }
+  }
+}
+
+var showLocalWeather = function(){
+  setAllCursors('progress');
+  navigator.geolocation.getCurrentPosition(function(position){
+    setAllCursors('auto');
+    var latlng = JSON.stringify({lat: position.coords.latitude, lng: position.coords.longitude});
+    var url = '/weather/geolocation/' + latlng;
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = function(){
+      if (request.status === 200) {
+        var data = JSON.parse(request.responseText);
+        city = new City(data.name);
+        city.data = data;
+        displayWeatherData(city);
+      }
+    };
+    request.send(null);
+  });
+};
+
+window.onload = function(){
+  var form = document.getElementById('weather-form');
+  var input = document.getElementById('city-input');
+  var loadButton = document.getElementById('quick-load');
+  var localButton = document.getElementById('get-location');
+  loadButton.onclick = function(){
+    cityName = JSON.parse(localStorage.getItem('city'));
+    input.value = cityName;
+    city = new City(cityName);
+    city.getWeatherData(function(){
+      displayWeatherData(city);
+    });
+  };
+  form.onsubmit = function(event){
+    event.preventDefault(); 
+    city = new City(input.value);
+    city.getWeatherData(function(){
+      displayWeatherData(city);
+    });
+  };
+  localButton.onclick = showLocalWeather;
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var expressLayouts = require('express-ejs-layouts');
+var http = require('http');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+var APIkey = '9a448cb3ffed441801362492ef3114d8';
+var units = 'metric';
+
+app.get('/weather', function(req, res){
+  res.render('weather');
+});
+
+app.get('/weather/:city', function(req, res){
+  http.get('http://api.openweathermap.org/data/2.5/weather?q=' + req.params.city + '&appid=' + APIkey + '&units=' + units, function(response){
+    var body = '';                                                                                                                        
+    response.on('data', function(d){
+      body += d;                                                                                                                          
+    });
+    response.on('end', function(){                                                                                                        
+      res.send(JSON.parse(body));
+    });
+  });
+});
+
+app.get('/weather/geolocation/:latlng', function(req, res){
+  var latlng = JSON.parse(req.params.latlng);
+  http.get('http://api.openweathermap.org/data/2.5/weather?lat=' + latlng.lat + '&lon=' + latlng.lng + '&appid=' + APIkey + '&units=' + units, function(response){
+    var body = '';                                                                                                                        
+    response.on('data', function(d){
+      body += d;                                                                                                                          
+    });
+    response.on('end', function(){                                                                                                        
+      res.send(JSON.parse(body));
+    });
+  });
+});
+
+app.listen('3000', function(){
+  console.log('App started on port 3000, access the page by browsing to http://localhost:3000');
+});
